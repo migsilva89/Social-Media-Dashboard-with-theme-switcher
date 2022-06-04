2c920cea0e552c0e3a077aaf3105199bbf336a88
migsilva89 <migdrum@gmail.com>
2022-06-04 23:33:08 +0200

    first section done with responsive aswell and 2nd section mobile version done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.25em',
+      }
+    },
   },
   plugins: [],
 }
